c11b0af76aa61124255cc02c818ee6025e6d4de1
TGAtharv <tgatharv09@gmail.com>
2024-10-18 22:06:05 +0530

    complete.py

---
+++ DateTime.py
@@ -0,0 +1,10 @@
+import datetime
+import time
+
+current_time = datetime.datetime.now()
+
+print("Time now at greenwhich meridian is:",end="")
+print(current_time)
+
+import calender
+print("\n",calender.calende(2024))
